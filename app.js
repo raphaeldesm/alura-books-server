d6a50c7ae10af43dce44bc988b6a44026b4d8d5d
Raphael de Souza Marinho <108803673+raphaeldesm@users.noreply.github.com>
2026-08-13 19:47:57 +0000

    Aula 5

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require("express")
 const rotaLivro = require("./rotas/livro")
 
-const app = express();
+const app = express()
+app.use(express.json())
 
 app.use('/livros', rotaLivro)
 
-const port = 8000;
+const port = 8000
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`);
--- controladores/livro.js
+++ controladores/livro.js
@@ -1,6 +1,80 @@
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } = require("../servicos/livro")
+
 function getLivros(req, res) {
     try {
-        res.send("Olá mundo da Alura do Brasil!")
+        const livros = getTodosLivros()
+        res.send(livros)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body
+        if(req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
+        
+    } catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
+    } catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
+        } else {
+        res.status(422)
+        res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -8,5 +82,9 @@ function getLivros(req, res) {
 }
 
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
